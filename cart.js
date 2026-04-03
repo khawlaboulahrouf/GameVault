29e24e7d96e93494cb226d89908752983ab7b2ec
khawlaboulahrouf <khawlaboulahrouf@gmail.com>
2026-04-03 16:25:56 +0100

    update cart.js

--- cart.js
+++ cart.js
@@ -1,29 +1,19 @@
 export let cart = loadCart();
 
-
-export function clearCart() {
-  cart.length = 0;
-  localStorage.removeItem("cart");
-}
-
-// 💾 load from localStorage
 function loadCart() {
   const data = localStorage.getItem("cart");
   return data ? JSON.parse(data) : [];
 }
 
-
-// 💾 save to localStorage
 function saveCart() {
   localStorage.setItem("cart", JSON.stringify(cart));
 }
 
-// ➕ ajouter
 export function addToCart(game) {
   const exist = cart.find(item => item.id === game.id);
 
   if (exist) {
-    exist.quantity += 1;
+    exist.quantity++;
   } else {
     cart.push({ ...game, quantity: 1 });
   }
@@ -31,21 +21,17 @@ export function addToCart(game) {
   saveCart();
 }
 
-// ➕ increase
 export function increaseQty(id) {
   const item = cart.find(p => p.id === id);
   if (item) item.quantity++;
-
   saveCart();
 }
 
-// ➖ decrease
 export function decreaseQty(id) {
   const item = cart.find(p => p.id === id);
 
   if (item) {
     item.quantity--;
-
     if (item.quantity <= 0) {
       cart = cart.filter(p => p.id !== id);
     }
@@ -54,9 +40,12 @@ export function decreaseQty(id) {
   saveCart();
 }
 
-// 🗑️ remove
 export function removeFromCart(id) {
   cart = cart.filter(p => p.id !== id);
-
   saveCart();
+}
+
+export function clearCart() {
+  cart.length = 0;
+  localStorage.removeItem("cart");
 }
